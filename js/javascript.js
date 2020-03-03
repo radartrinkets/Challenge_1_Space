cddc4c6e7dad884956f3636c8223fb124e29f90a
radartrinkets <navdmarel@gmail.com>
2020-03-03 14:11:34 +0100

    grafieken werkend

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,82 @@
 google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+      google.charts.setOnLoadCallback(drawChart1);
 
-      function drawChart() {
+      function drawChart1() {
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
+          ['Time in hours', 'Speed'],
+          ['2004',  1000],
+          ['2005',  1170],
+          ['2006',  660],
+          ['2007',  1030]
         ]);
 
         var options = {
-          title: 'Company Performance',
+          
           curveType: 'function',
-          legend: { position: 'bottom' }
+          hAxis: {title: 'Time in hours', titleTextStyle: {color: 'white'}},
+          vAxis: {title: 'km/h', textStyle: {color: 'white', fontName:'Josefin Sans'}, titleTextStyle: {color: 'white'}},
+          backgroundColor: '#212121',
+          titleTextStyle: { color: 'white',
+                            fontName: 'Josefin Sans',
+                            bold: 'false'}
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        var chart = new google.visualization.AreaChart(document.getElementById('speed'));
 
         chart.draw(data, options);
+      }
+
+
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart2);
+       function drawChart2() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          
+          
+          pieHole: 0.4,
+          backgroundColor: '#212121',
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('atmosfeer'));
+        chart.draw(data, options);
+      }
+
+
+
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart3);
+
+      function drawChart3() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Memory', 80],
+         
+        ]);
+
+        var options = {
+          width: 400, height: 120,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('gravity'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 13000);
+        
       }
